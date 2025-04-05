71dab67a52d1320c2205b067608b8d5501415062
桜田美緒 <121525028+sakurada0291@users.noreply.github.com>
2025-04-05 09:47:34 +0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-import { Buffer } from 'buffer';
+import {
+    Buffer
+} from 'buffer';
 
 import $ from 'umbrellajs';
 import * as d3 from 'd3';
@@ -7,15 +9,29 @@ import chardet from 'chardet';
 import iconv from 'iconv-lite';
 
 import parseTJA from './parseTJA';
-import { getCourseLines, getEnabledBranch } from './parseTJA';
+import {
+    getCourseLines,
+    getEnabledBranch
+} from './parseTJA';
 import drawChart from './drawChart';
-import { initUsedSprite } from './drawChart';
+import {
+    initUsedSprite
+} from './drawChart';
 import analyseChart from './analyseChart';
-import { predictScore } from './analyseChart';
-import { embedText, optimizePNG } from './embedChart';
-import { convertToDonscore } from './donscore';
+import {
+    predictScore
+} from './analyseChart';
+import {
+    embedText,
+    optimizePNG
+} from './embedChart';
+import {
+    convertToDonscore
+} from './donscore';
 
-import { loadAllFonts } from './font/font';
+import {
+    loadAllFonts
+} from './font/font';
 
 import '../css/style.scss';
 import '../css/font/Pixel-3x5.css';
@@ -59,7 +75,7 @@ function updateUI() {
     else hidePreview();
 
     if (selectedPage === 'statistics') showStatistics();
-	
+
     $('.controls-branch .button.is-active').removeClass('is-active');
     $(`.controls-branch .btn-branch-${selectedBranch.toLowerCase()}`).addClass('is-active');
 }
@@ -84,27 +100,26 @@ function showPreview() {
     if (selectedDifficulty === '') return;
 
     $('#tja-preview').remove();
-	
-	try {
-		const $canvas = drawChart(tjaParsed, selectedDifficulty);
-		const $img =  document.createElement('img');
-		$img.id =  'tja-preview';
-		const chartImg = optimizePNG($canvas);
-		
-		if ($embedDonscore.checked) {
-			$img.src = embedText(chartImg, convertToDonscore(tjaParsed, selectedDifficulty));
-		}
-		else {
-			$img.src = embedText(chartImg, getCourseLines($input.first().value, selectedDifficulty));
-		}
-		
-		$('.page-preview').append($img);
 
-		displayErrors('No error');
-	} catch (e) {
-		console.error(e);
-		displayErrors(e.message);
-	}
+    try {
+        const $canvas = drawChart(tjaParsed, selectedDifficulty);
+        const $img = document.createElement('img');
+        $img.id = 'tja-preview';
+        const chartImg = optimizePNG($canvas);
+
+        if ($embedDonscore.checked) {
+            $img.src = embedText(chartImg, convertToDonscore(tjaParsed, selectedDifficulty));
+        } else {
+            $img.src = embedText(chartImg, getCourseLines($input.first().value, selectedDifficulty));
+        }
+
+        $('.page-preview').append($img);
+
+        displayErrors('No error');
+    } catch (e) {
+        console.error(e);
+        displayErrors(e.message);
+    }
 }
 
 function hidePreview() {
@@ -114,12 +129,12 @@ function hidePreview() {
 function showStatistics() {
     if (selectedDifficulty === '') return;
 
-	const enabledBranch = getEnabledBranch(tjaParsed, selectedDifficulty);
+    const enabledBranch = getEnabledBranch(tjaParsed, selectedDifficulty);
 
-	$('.controls-branch .button').addClass('is-hidden');
-	for (let branch of enabledBranch) {
-		$(`.controls-branch .btn-branch-${branch.toLowerCase()}`).removeClass('is-hidden');
-	}
+    $('.controls-branch .button').addClass('is-hidden');
+    for (let branch of enabledBranch) {
+        $(`.controls-branch .btn-branch-${branch.toLowerCase()}`).removeClass('is-hidden');
+    }
 
     try {
         const data = analyseChart(tjaParsed, selectedDifficulty, selectedBranch);
@@ -131,136 +146,140 @@ function showStatistics() {
 }
 
 export function toFixedZero(num) {
-	let newNum = num;
-	while (true) {
-		if (newNum.charAt(newNum.length - 1) === '0') {
-			newNum = newNum.slice(0, -1);
-		} else if (newNum.charAt(newNum.length - 1) === '.') {
-			newNum = newNum.slice(0, -1);
-			break;
-		} else {
-			break;
-		}
-	}
-    
+    let newNum = num;
+    while (true) {
+        if (newNum.charAt(newNum.length - 1) === '0') {
+            newNum = newNum.slice(0, -1);
+        } else if (newNum.charAt(newNum.length - 1) === '.') {
+            newNum = newNum.slice(0, -1);
+            break;
+        } else {
+            break;
+        }
+    }
+
     return newNum;
 }
 
 function buildStatisticsPage(data) {
-    const { statistics: stats, graph } = data;
+    const {
+        statistics: stats,
+        graph
+    } = data;
 
     // Statistics
     $('.stat-total-combo').text(stats.totalCombo);
 
     const course = tjaParsed.courses[selectedDifficulty];
-    let { scoreInit, scoreDiff, scoreShin } = course.headers;
-	if (selectedCalcMode === 'predict') {
-		const predicted = predictScore(stats, course, selectedGogoFloor, selectedScoreSystem);
-		scoreInit = predicted[0];
-		scoreDiff = predicted[1];
-		scoreShin = predicted[2];
-	}
+    let {
+        scoreInit,
+        scoreDiff,
+        scoreShin
+    } = course.headers;
+    if (selectedCalcMode === 'predict') {
+        const predicted = predictScore(stats, course, selectedGogoFloor, selectedScoreSystem);
+        scoreInit = predicted[0];
+        scoreDiff = predicted[1];
+        scoreShin = predicted[2];
+    }
 
-	$('.stat-level').text('★×' + course.headers.level);
+    $('.stat-level').text('★×' + course.headers.level);
 
     const drop1 = n => Math.floor(n / 10) * 10;
     const multipliers = [0, 1, 2, 4, 8];
-	const noteScores = multipliers.map(m => drop1(scoreInit + scoreDiff * m));
-	const noteScores2 = multipliers.map(m => (scoreInit + scoreDiff * m));
-	const noteScoresShin = multipliers.map(m => scoreShin);
-	const noteScoresBig = multipliers.map(m => drop1(scoreInit + scoreDiff * m) * 2);
-	
-	let noteGogoScores;
-	let noteGogoScoresBig;
-	if (selectedGogoFloor === 'AC15') {
-		noteGogoScores = noteScores.map(s => drop1(s * 1.2));
-		noteGogoScoresBig = noteScores.map(s => drop1(s * 1.2) * 2);
-	}
-    else {
-		noteGogoScores = noteScores2.map(s => drop1(s * 1.2));
-		noteGogoScoresBig = noteScores2.map(s => drop1(s * 1.2) * 2);
-	}
-	
-	let statPotential;
-	let statPotential2;
-	if (selectedScoreSystem != 'AC16New') {
-		statPotential = (
-			noteScores.map((s, i) => stats.score.notes[0][0][i] * s).reduce((p, c) => p + c, 0) +
-			noteGogoScores.map((s, i) => stats.score.notes[0][1][i] * s).reduce((p, c) => p + c, 0) +
-			noteScoresBig.map((s, i) => stats.score.notes[1][0][i] * s).reduce((p, c) => p + c, 0) +
-			noteGogoScoresBig.map((s, i) => stats.score.notes[1][1][i] * s).reduce((p, c) => p + c, 0) +
-			stats.score.balloon[0] * 300 +
-			stats.score.balloon[1] * 360 +
-			stats.score.balloonPop[0] * 5000 +
-			stats.score.balloonPop[1] * 6000 +
-			Math.floor(stats.totalCombo / 100) * 10000
-		);
-		if (scoreShin != null) {
-			if (selectedScoreSystem === 'CS') {
-				statPotential2 = ((stats.totalCombo + (stats.notes[2] + stats.notes[3])) * scoreShin) +
-								 (stats.score.balloon[0] * 300) +
-								 (stats.score.balloon[1] * 300) +
-								 (stats.score.balloonPop[0] * 5000) +
-								 (stats.score.balloonPop[1] * 5000);
-			}
-			else if (selectedScoreSystem === 'AC16Old') {
-				statPotential2 = (stats.totalCombo * scoreShin) +
-								 (stats.score.balloon[0] * 100) +
-								 (stats.score.balloon[1] * 100) +
-								 (stats.score.balloonPop[0] * 100) +
-								 (stats.score.balloonPop[1] * 100);
-			}
-		}
-	}
-	else {
-		statPotential = (stats.totalCombo * scoreInit) +
-						(stats.score.balloon[0] * 100) +
-						(stats.score.balloon[1] * 100) +
-						(stats.score.balloonPop[0] * 100) +
-						(stats.score.balloonPop[1] * 100);
-	}
+    const noteScores = multipliers.map(m => drop1(scoreInit + scoreDiff * m));
+    const noteScores2 = multipliers.map(m => (scoreInit + scoreDiff * m));
+    const noteScoresShin = multipliers.map(m => scoreShin);
+    const noteScoresBig = multipliers.map(m => drop1(scoreInit + scoreDiff * m) * 2);
 
-	if (selectedScoreSystem != 'AC16New') {
-		if (stats.rendas.length) $('.stat-max-score').text(`${scoreInit}点, ${scoreDiff}点 => ${statPotential}点 + 連打`);
-		else $('.stat-max-score').text(`${scoreInit}点, ${scoreDiff}点 => ${statPotential}点`);
-		if (scoreShin != null) {
-			if (stats.rendas.length) $('.stat-max-score2').text(`${scoreShin}点 => ${statPotential2}点 + 連打`);
-			else $('.stat-max-score2').text(`${scoreShin}点 => ${statPotential2}点`);
-		}
-		else {
-			$('.stat-max-score2').text('');
-		}
-	}
-	else {
-		if (stats.rendas.length) $('.stat-max-score').text(`${scoreInit}点 => ${statPotential}点 + 連打`);
-		else $('.stat-max-score').text(`${scoreInit}点 => ${statPotential}点`);
-		$('.stat-max-score2').text('');
-	}
-	
-	let bpmMin = 0, bpmMax = 0, firstBpm = true;
-	for (let i = 0; i < course.measures.length; i++) {
-		for (let j = 0; j < course.measures[i].events.length; j++) {
-			if (course.measures[i].events[j].name === 'bpm') {
-				let curBpm = parseFloat(course.measures[i].events[j].value);
-				
-				if (firstBpm) {
-					bpmMin = curBpm;
-					bpmMax = curBpm;
-					firstBpm = false;
-				} else {
-					if (bpmMin > curBpm) {
-						bpmMin = curBpm;
-					}
-					if (bpmMax < curBpm) {
-						bpmMax = curBpm;
-					}
-				}
-			}
-		}
-	}
-	if (bpmMin.toFixed(2) != bpmMax.toFixed(2)) $('.stat-bpm').text(toFixedZero(bpmMin.toFixed(2)) + '-' + toFixedZero(bpmMax.toFixed(2)));
-	else $('.stat-bpm').text(toFixedZero(bpmMax.toFixed(2)));
-	
+    let noteGogoScores;
+    let noteGogoScoresBig;
+    if (selectedGogoFloor === 'AC15') {
+        noteGogoScores = noteScores.map(s => drop1(s * 1.2));
+        noteGogoScoresBig = noteScores.map(s => drop1(s * 1.2) * 2);
+    } else {
+        noteGogoScores = noteScores2.map(s => drop1(s * 1.2));
+        noteGogoScoresBig = noteScores2.map(s => drop1(s * 1.2) * 2);
+    }
+
+    let statPotential;
+    let statPotential2;
+    if (selectedScoreSystem != 'AC16New') {
+        statPotential = (
+            noteScores.map((s, i) => stats.score.notes[0][0][i] * s).reduce((p, c) => p + c, 0) +
+            noteGogoScores.map((s, i) => stats.score.notes[0][1][i] * s).reduce((p, c) => p + c, 0) +
+            noteScoresBig.map((s, i) => stats.score.notes[1][0][i] * s).reduce((p, c) => p + c, 0) +
+            noteGogoScoresBig.map((s, i) => stats.score.notes[1][1][i] * s).reduce((p, c) => p + c, 0) +
+            stats.score.balloon[0] * 300 +
+            stats.score.balloon[1] * 360 +
+            stats.score.balloonPop[0] * 5000 +
+            stats.score.balloonPop[1] * 6000 +
+            Math.floor(stats.totalCombo / 100) * 10000
+        );
+        if (scoreShin != null) {
+            if (selectedScoreSystem === 'CS') {
+                statPotential2 = ((stats.totalCombo + (stats.notes[2] + stats.notes[3])) * scoreShin) +
+                    (stats.score.balloon[0] * 300) +
+                    (stats.score.balloon[1] * 300) +
+                    (stats.score.balloonPop[0] * 5000) +
+                    (stats.score.balloonPop[1] * 5000);
+            } else if (selectedScoreSystem === 'AC16Old') {
+                statPotential2 = (stats.totalCombo * scoreShin) +
+                    (stats.score.balloon[0] * 100) +
+                    (stats.score.balloon[1] * 100) +
+                    (stats.score.balloonPop[0] * 100) +
+                    (stats.score.balloonPop[1] * 100);
+            }
+        }
+    } else {
+        statPotential = (stats.totalCombo * scoreInit) +
+            (stats.score.balloon[0] * 100) +
+            (stats.score.balloon[1] * 100) +
+            (stats.score.balloonPop[0] * 100) +
+            (stats.score.balloonPop[1] * 100);
+    }
+
+    if (selectedScoreSystem != 'AC16New') {
+        if (stats.rendas.length) $('.stat-max-score').text(`${scoreInit}点, ${scoreDiff}点 => ${statPotential}点 + 連打`);
+        else $('.stat-max-score').text(`${scoreInit}点, ${scoreDiff}点 => ${statPotential}点`);
+        if (scoreShin != null) {
+            if (stats.rendas.length) $('.stat-max-score2').text(`${scoreShin}点 => ${statPotential2}点 + 連打`);
+            else $('.stat-max-score2').text(`${scoreShin}点 => ${statPotential2}点`);
+        } else {
+            $('.stat-max-score2').text('');
+        }
+    } else {
+        if (stats.rendas.length) $('.stat-max-score').text(`${scoreInit}点 => ${statPotential}点 + 連打`);
+        else $('.stat-max-score').text(`${scoreInit}点 => ${statPotential}点`);
+        $('.stat-max-score2').text('');
+    }
+
+    let bpmMin = 0,
+        bpmMax = 0,
+        firstBpm = true;
+    for (let i = 0; i < course.measures.length; i++) {
+        for (let j = 0; j < course.measures[i].events.length; j++) {
+            if (course.measures[i].events[j].name === 'bpm') {
+                let curBpm = parseFloat(course.measures[i].events[j].value);
+
+                if (firstBpm) {
+                    bpmMin = curBpm;
+                    bpmMax = curBpm;
+                    firstBpm = false;
+                } else {
+                    if (bpmMin > curBpm) {
+                        bpmMin = curBpm;
+                    }
+                    if (bpmMax < curBpm) {
+                        bpmMax = curBpm;
+                    }
+                }
+            }
+        }
+    }
+    if (bpmMin.toFixed(2) != bpmMax.toFixed(2)) $('.stat-bpm').text(toFixedZero(bpmMin.toFixed(2)) + '-' + toFixedZero(bpmMax.toFixed(2)));
+    else $('.stat-bpm').text(toFixedZero(bpmMax.toFixed(2)));
+
     $('.stat-don-small').text(stats.notes[0]);
     $('.stat-don-big').text(stats.notes[2]);
     $('.stat-kat-small').text(stats.notes[1]);
@@ -287,7 +306,8 @@ function buildStatisticsPage(data) {
     )).join('<br>'));
 
     // Graph
-    const graphWidth = 600, graphHeight = 200;
+    const graphWidth = 600,
+        graphHeight = 200;
     const x = d3.scaleBand().rangeRound([0, graphWidth]);
     const y = d3.scaleLinear().rangeRound([graphHeight, 0]);
     const yMax = Math.ceil(graph.max / 5) * 5;
@@ -333,54 +353,56 @@ function buildStatisticsPage(data) {
 }
 
 function copyRendaText(rendas, rendaExtends) {
-	let result = '', groupCount = 0, groupFirst = true;
-	const groupMax = rendaExtends.reduce((a, b) => Math.max(a, b.rendaGroup), -1);
-	
-	for (let i = 0; i < rendas.length; i++) {
-		if (rendaExtends[i].rendaGroup != groupCount) {
-			groupCount += 1;
-			groupFirst = true;
-		}
-		
-		if (groupFirst) {
-			if (rendaExtends[i].isBigRenda) {
-				result += 'SIZE(16){';
-			}
-			
-			if (rendaExtends[i].isGoGoRenda) {
-				result += '\'\'';
-			}
-			
-			result += '約' + rendas[i].toFixed(3) + '秒'
-			
-			if (rendaExtends[i].isGoGoRenda) {
-				result += '\'\'';
-			}
-			
-			if (rendaExtends[i].isBigRenda) {
-				result += '}';
-			}
-			
-			let groupNum = rendaExtends.reduce((a, b) => (b.rendaGroup === groupCount ? a + 1 : a), 0);
-			if (groupNum > 1) {
-				result += '×' + groupNum;
-			}
-			
-			if (rendaExtends[i].rendaGroup != groupMax) {
-				result += '－';
-			}
-			
-			groupFirst = false;
-		}
-	}
-	
-	if (rendas.length > 1) {
-		result += '： 合計約' + rendas.reduce((a, b) => a + b, 0).toFixed(3) + '秒';
-	}
-	
-	if (result != '') {
-		navigator.clipboard.writeText(result);
-	}
+    let result = '',
+        groupCount = 0,
+        groupFirst = true;
+    const groupMax = rendaExtends.reduce((a, b) => Math.max(a, b.rendaGroup), -1);
+
+    for (let i = 0; i < rendas.length; i++) {
+        if (rendaExtends[i].rendaGroup != groupCount) {
+            groupCount += 1;
+            groupFirst = true;
+        }
+
+        if (groupFirst) {
+            if (rendaExtends[i].isBigRenda) {
+                result += 'SIZE(16){';
+            }
+
+            if (rendaExtends[i].isGoGoRenda) {
+                result += '\'\'';
+            }
+
+            result += '約' + rendas[i].toFixed(3) + '秒'
+
+            if (rendaExtends[i].isGoGoRenda) {
+                result += '\'\'';
+            }
+
+            if (rendaExtends[i].isBigRenda) {
+                result += '}';
+            }
+
+            let groupNum = rendaExtends.reduce((a, b) => (b.rendaGroup === groupCount ? a + 1 : a), 0);
+            if (groupNum > 1) {
+                result += '×' + groupNum;
+            }
+
+            if (rendaExtends[i].rendaGroup != groupMax) {
+                result += '－';
+            }
+
+            groupFirst = false;
+        }
+    }
+
+    if (rendas.length > 1) {
+        result += '： 合計約' + rendas.reduce((a, b) => a + b, 0).toFixed(3) + '秒';
+    }
+
+    if (result != '') {
+        navigator.clipboard.writeText(result);
+    }
 }
 
 //==============================================================================
@@ -391,11 +413,14 @@ $editorProcess.on('click', () => {
 });
 
 $rendaHead.on('click', () => {
-	if (selectedDifficulty === '') return;
-	
+    if (selectedDifficulty === '') return;
+
     const data = analyseChart(tjaParsed, selectedDifficulty, selectedBranch);
-	const { statistics: stats, graph } = data;
-	copyRendaText(stats.rendas, stats.rendaExtends);
+    const {
+        statistics: stats,
+        graph
+    } = data;
+    copyRendaText(stats.rendas, stats.rendaExtends);
 });
 
 $input.on('input', () => {
@@ -457,10 +482,10 @@ $('.controls-diff .button').on('click', evt => {
     const diff = $(evt.target).data('value');
 
     selectedDifficulty = diff;
-	
-	const enabledBranch = getEnabledBranch(tjaParsed, selectedDifficulty);
-	selectedBranch = enabledBranch[enabledBranch.length - 1];
-	
+
+    const enabledBranch = getEnabledBranch(tjaParsed, selectedDifficulty);
+    selectedBranch = enabledBranch[enabledBranch.length - 1];
+
     updateUI();
 });
 
@@ -480,47 +505,43 @@ $('.controls-page .button').on('click', evt => {
 
 $('.controls-score-system .radio').on('click', evt => {
     const name = evt.target.name;
-	const value = evt.target.value;
-	
-	if (name === 'scoreSystem') {
-		selectedScoreSystem = value;
-	}
-	else if (name === 'gogoFloor'){
-		selectedGogoFloor = value;
-	}
-	else if (name === 'calcMode'){
-		selectedCalcMode = value;
-	}
-	
+    const value = evt.target.value;
+
+    if (name === 'scoreSystem') {
+        selectedScoreSystem = value;
+    } else if (name === 'gogoFloor') {
+        selectedGogoFloor = value;
+    } else if (name === 'calcMode') {
+        selectedCalcMode = value;
+    }
+
     updateUI();
 });
 
 $('.download-donscore .button').on('click', async evt => {
-	if (selectedDifficulty === '') return;
-	
-	const fh = await window.showSaveFilePicker({
-		types: [
-			{
-				description: 'Text File',
-				accept: {
-					'text/plain': ['.txt'],
-				},
-			},
-		],
-	});
-	
-	const writable = await fh.createWritable();
-	const writeText = convertToDonscore(tjaParsed, selectedDifficulty);
-	
-	const sjisText = iconv.encode(writeText, 'shift_jis');
-	await writable.write(sjisText);
-	await writable.close();
-	
+    if (selectedDifficulty === '') return;
+
+    const fh = await window.showSaveFilePicker({
+        types: [{
+            description: 'Text File',
+            accept: {
+                'text/plain': ['.txt'],
+            },
+        }, ],
+    });
+
+    const writable = await fh.createWritable();
+    const writeText = convertToDonscore(tjaParsed, selectedDifficulty);
+
+    const sjisText = iconv.encode(writeText, 'shift_jis');
+    await writable.write(sjisText);
+    await writable.close();
+
 });
 
 window.onload = async function() {
-	await initUsedSprite();
-	await loadAllFonts();
+    await initUsedSprite();
+    await loadAllFonts();
 }
 
 //==============================================================================
